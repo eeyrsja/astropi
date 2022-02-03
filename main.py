af64a5dc09864450e6e6859b95084b9d4312ff55
stu <stujalex@outlook.com>
2022-02-03 16:27:11 +0000

    Some tidying up!

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
-# Main Program for JBVK Astro Pi Project
-# Helpful information:
-# https://projects.raspberrypi.org/en/projects/code-for-your-astro-pi-mission-space-lab-experiment/2
+# Main Python Script for JBVK Astro Pi Project
+# --------------------------------------------
+# JBVK:
+#   Jack McKinnon
+#   Ben Alexander
+#   Victor Florek
+#   Kieran Pennalls
+
+# Hello from Balsall Common!
 
 import csv
 from pathlib import Path
 from datetime import datetime, timedelta
 from logzero import logger, logfile
-#from picamera import PiCamera
-#from orbit import ISS
-#from skyfield.api import load
+from picamera import PiCamera
+from orbit import ISS
+from skyfield.api import load
 from time import sleep
 
 # Running time information
@@ -22,12 +28,12 @@ data_file = base_folder/'data.csv'
 logfile(base_folder/"events.log")
 
 # Parameters for ISS position finding
-#ephemeris = load('de421.bsp') TODO - uncomment to run on astropi
-#timescale = load.timescale() TODO - uncomment to run on astropi
+ephemeris = load('de421.bsp')
+timescale = load.timescale()
 
 # Set up the camera
-#camera = PiCamera() TODO - uncomment to run on astropi
-#camera.resolution = (1296,972) TODO - uncomment to run on astropi
+camera = PiCamera()
+camera.resolution = (1296, 972)
 
 
 def create_csv(data_file):
@@ -36,40 +42,52 @@ def create_csv(data_file):
 
     csv files done by Ben Alexander
 
-    :param data_file:
-    :return:
+    :param data_file: the name of the file to save the results in
+    :return: True or false if it can make the new file
     """
-    with open(data_file, 'w') as f:
-        writer = csv.writer(f)
-        header = ("Time", "Day_or_Night", "Latitude", "Longitude", "Cloud_Percent", "Photo_Link")
-        writer.writerow(header)
+    try:
+        with open(data_file, 'w') as f:
+            writer = csv.writer(f)
+            header = ("Time", "Day_or_Night", "Latitude", "Longitude", "Cloud_Percent", "Photo_File")
+            writer.writerow(header)
+        return True
+    except:
+        #the file could not be made
+        return False
 
 
-# results
-
-
-def add_results_to_csv(data_file, Time, Day_or_Night, Latitude, Longitude, Cloud_Percent, Photo_Link):
+def add_results_to_csv(data_file, time, day_or_night, latitude, longitude, cloud_percent, photo_file):
     """
     this function puts the data in a .csv file.
     
     csv files done by Ben Alexander
 
-    :param data_file:
-    :param Time:
-    :param Day_or_Night:
-    :param Latitude: 
-    :param Longitude: 
-    :param Cloud_Percent:
-    :param Photo_Link: 
-    :return: 
+    :param data_file: the name of the file to save the results in
+    :param time: the actual time when data collected
+    :param day_or_night: is it daytime or night time?
+    :param latitude: location of ISS
+    :param longitude: location of ISS
+    :param cloud_percent: our estimate of the cloud cover in the photo
+    :param photo_file: link to the photo
+    :return: True or false if it can add the result to the file
     """
-    with open(data_file, 'a') as f:
-        writer = csv.writer(f)
-        result = (Time, Day_or_Night, Latitude, Longitude, Cloud_Percent, Photo_Link)
-        writer.writerow(result)
+    try:
+        with open(data_file, 'a') as f:
+            writer = csv.writer(f)
+            result = (time, day_or_night, latitude, longitude, cloud_percent, photo_file)
+            writer.writerow(result)
+        return True
+    except:
+        # the data could not be added
+        return False
 
 
 def get_iss_position():
+    """
+    this function uses the ISS library to get the current latitude and longitude of the ISS in degrees
+
+    :return: lat, long
+    """
     location = ISS.coordinates()
     latitude = location.latitude.degrees
     longitude = location.longitude.degrees
@@ -77,6 +95,11 @@ def get_iss_position():
 
 
 def get_day_night():
+    """
+    this function uses the is_sunlit function to find out if it is day or night
+
+    :return: day or night
+    """
     t = timescale.now()
     if ISS.at(t).is_sunlit(ephemeris):
         return "day"
@@ -89,24 +112,29 @@ def get_cloud_percent(photo):
     return 20
 
 
+# MAIN TASK
+# ---------
+
 # Set up the output file
 create_csv(data_file)
 counter =1
 
-# Main program loop for 3 hours
+# Main program loop for just less than 3 hours (180 minutes)
 while (now_time < start_time + timedelta(minutes=2)): #TODO change to 175 minutes
     # Take a photo and save it
+    photo_file = base_folder/f'photo_{counter:04}.jpg'
     camera.start_preview()
     sleep(2) # Camera warm-up time
-    camera.capture(f"{base_folder}/pic_{counter:04}.jpg")
+    camera.capture()
+
     # Get measurements
     current_datetime = datetime.now().strftime("%Y-%m-%d %H:%M")
     day_or_night = get_day_night()
     iss_position = get_iss_position()
-    cloud_percent = get_cloud_percent(f"{base_folder}/pic_{counter:04}.jpg")
+    cloud_percent = get_cloud_percent(photo_file)
 
     # Write the results to CSV file
-    add_results_to_csv(data_file, current_datetime, day_or_night, iss_position[0], iss_position[1], cloud_percent,"PHOTO1.JPG")
+    add_results_to_csv(data_file, current_datetime, day_or_night, iss_position[0], iss_position[1], cloud_percent, photo_file)
 
     # Log the event
     logger.info(f"main loop counter: {counter}")
