75a3c3c702696485a3b50d42095db8a57a252882
stu <stujalex@outlook.com>
2022-02-03 20:17:34 +0000

    reorg

--- ygui.py
+++
@@ -1,5 +0,0 @@
-out = im.transpose(Image.FLIP_LEFT_RIGHT)
-out = im.transpose(Image.FLIP_TOP_BOTTOM)
-out = im.transpose(Image.ROTATE_90)
-out = im.transpose(Image.ROTATE_180)
-out = im.transpose(Image.ROTATE_270)
